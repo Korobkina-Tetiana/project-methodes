61dc69ebf981196fecf73591dae5e2f59bc1e18d
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-10 17:34:54 +0200

    add created a simple couunter in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+interface CounterProps {
+  count: number;
+}
 
+function Counter({ count }: CounterProps) {
+  return (<div>
+    <p>Count: {count}</p>
+  </div>)
+}
+ 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <Counter count={count}/>
+        <button onClick={() => setCount(count - 1)}>-</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
